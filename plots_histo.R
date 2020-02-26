1880175bef83e613f90ab69ecdbef818ecf1e1b9
qjutard <quentin.jutard@locean-ipsl.upmc.fr>
2020-02-26 12:20:39 +0100

    added labels to some plots

--- plots_histo.R
+++ plots_histo.R
@@ -73,7 +73,7 @@ zone_sample = ALL_profiles$zone[sample]
 
 cutoff = -0.02
 
-mirrored_hist(off_auto_sample[which(auto_to_med_sample<cutoff)], off_med_sample[which(auto_to_med_sample<cutoff)], 70)
+mirrored_hist(off_auto_sample[which(auto_to_med_sample<cutoff)], off_med_sample[which(auto_to_med_sample<cutoff)], 10, "mg/m³", "Automatic", "Median")
 
 negative_diff_floats = unique(str_sub(prof_name_sample[which(auto_to_med_sample<cutoff & off_auto_sample==0)],3,9))
 
@@ -84,19 +84,19 @@ plot_profiles = data.frame(diff=auto_to_med_sample, long=ALL_profiles$lon[sample
 cutoff=-0.02
 plot_profiles2 = data.frame(diff=auto_to_med_sample[which(auto_to_med_sample>=cutoff)], long=long[which(auto_to_med_sample>=cutoff)], lat=lat[which(auto_to_med_sample>=cutoff)])
 plot_profiles2$diff[which(plot_profiles2$diff<0)] = 0
-plot_profiles2$diff[which(plot_profiles2$diff>0.07)] = 0.07
+plot_profiles2$diff[which(plot_profiles2$diff>0.05)] = 0.05
 
-
+world_map <- map_data("world")
 map <- ggplot(world_map, aes(x = long, y = lat, group=group)) +
     geom_polygon(fill="darkgray") + theme_dark() +
-    scale_x_continuous(limits = c(-180,-70)) + 
-    scale_y_continuous(limits = c(-60,60))
+    scale_x_continuous(limits = c(-70,0)) + 
+    scale_y_continuous(limits = c(0,80))
     
 map + geom_point(data = plot_profiles2, aes(x = long, y = lat, group=NULL, color=diff), size=0.1) +
     #scale_color_gradient2(midpoint=0, low="blue", mid="white", high="red", space ="Lab" )
     scale_color_gradientn(colors=viridisLite::plasma(10))
 
-mirrored_hist <- function(x1, x2, br) {
+mirrored_hist <- function(x1, x2, br, xlab, ylab1, ylab2) {
     par(mfrow=c(2,1))
   
     hist1 = hist(x1, plot=F, breaks = br)
@@ -106,10 +106,10 @@ mirrored_hist <- function(x1, x2, br) {
     Yrange = range(c(hist1$counts, hist2$counts), na.rm=T)
     
     #Make the plot
-    par(mar=c(0,5,3,3))
-    hist(x1 , main="" , xlim=Xrange, xlab="", ylim=Yrange , xaxt="n", las=1 , col="slateblue1", breaks=br)
-    par(mar=c(5,5,0,3))
-    hist(x2 , main="" , xlim=Xrange, xlab="Value of my variable", ylim=rev(Yrange) , las=1 , col="tomato3"  , breaks=br)
+    par(mar=c(0.1,5,3,3))
+    hist(x1 , main="" , xlim=Xrange, xlab="", ylab=ylab1, ylim=Yrange , xaxt="n", las=1 , col="slateblue1", breaks=br)
+    par(mar=c(5,5,0.1,3))
+    hist(x2 , main="" , xlim=Xrange, xlab=xlab, ylab=ylab2, ylim=rev(Yrange) , las=1 , col="tomato3"  , breaks=br)
     
 }
 
